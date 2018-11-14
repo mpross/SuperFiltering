50f3c972c2accb6dcf4359e26984c32b1a8bb0b3
M.P.Ross <mpross2@uw.edu>
2018-11-14 11:55:35 -0800

    Logistic ridge regression on time series.

--- superClassify.py
+++ superClassify.py
@@ -29,6 +29,23 @@ def read_data(index):
     return time, wave_data, noise_data
 
 
+def logReg(x, y, lamb):
+
+    w = np.zeros(x.shape[0])
+
+    l = sum(np.log(1 + np.exp(-y * np.dot(x.T, w))))
+    print(l)
+
+    stepSize = 4.4275*10**-14
+    for i in range(10**3):
+        deltaL = np.dot(np.exp(-y * np.dot(x.T, w))/(1 + np.exp(-y * np.dot(x.T, w)))*-y, x.T) \
+                 + 2*lamb*np.sqrt(np.dot(w.T, w))
+        w = w - stepSize*deltaL
+        l = sum(np.log(1 + np.exp(-y * np.dot(x.T, w)))) + lamb*np.dot(w.T, w)
+        print(l)
+
+    return w
+
 x_train = np.zeros(4096)
 y_train = np.array(0)
 x_test = np.zeros(4096)
@@ -42,7 +59,7 @@ for i in range(1, len(os.listdir('./Data Sets/'))):
         x_train = np.column_stack((x_train, wave_data))
         y_train = np.append(y_train, 1)
         x_train = np.column_stack((x_train, noise_data))
-        y_train = np.append(y_train, 0)
+        y_train = np.append(y_train, -1)
 
     if i > len(os.listdir('./Data Sets/')) / 2:
         if os.path.isfile('Data Sets/signal' + str(i) + '.dat'):
@@ -51,10 +68,15 @@ for i in range(1, len(os.listdir('./Data Sets/'))):
         x_test = np.column_stack((x_test, wave_data))
         y_test = np.append(y_test, 1)
         x_test = np.column_stack((x_test, noise_data))
-        y_test = np.append(y_test, 0)
+        y_test = np.append(y_test, -1)
 
+trainErr = np.zeros(10)
+testErr = np.zeros(10)
+for i in range(10):
+    w = logReg(x_train, y_train, 0)
+    trainErr[i] = sum(y_train == np.dot(x_train.T,w))
+    testErr[i] = sum(y_test == np.dot(x_test.T, w))
 
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+plt.plot(range(10), trainErr)
+plt.plot(range(10), testErr)
+plt.show()
