f75383e6caaa72b12ddf0e689f3b7484a47add70
M.P.Ross <mpross2@uw.edu>
2018-11-16 11:58:54 -0800

    Removed homemade logistic regression and added SVM and SVD plots.

--- plots.py
+++ plots.py
@@ -14,14 +14,14 @@ def read_data(index):
 
     for i in range(0, l):
         time[i] = float(lines[i].split(' ')[0])
-        wave_data[i] = float(lines[i].split(' ')[1])*10**23
+        wave_data[i] = float(lines[i].split(' ')[1])
 
     f.close()
 
     f = open('Data Sets/noise' + str(index) + '.dat', 'r')
     lines = f.read().split('\n')
     for i in range(0, l):
-        noise_data[i] = float(lines[i].split(' ')[1])*10**23
+        noise_data[i] = float(lines[i].split(' ')[1])
 
     f.close()
 
@@ -30,18 +30,22 @@ def read_data(index):
 
 time, wave_data, noise_data = read_data(1)
 
+
 sampF = 1/(time[1]-time[0])
 
 plt.figure(1)
-plt.plot(time, wave_data,time,noise_data)
+plt.plot(time, wave_data, time, noise_data)
+plt.xlabel('Time (s)')
+plt.ylabel('Strain')
+plt.grid(True,'both')
 plt.draw()
 
 f, t, Sxx = signal.spectrogram(wave_data, sampF, 'hann', 100, 90)
 
 plt.figure(2)
 plt.pcolormesh(t, f, np.log10(Sxx))
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+plt.ylabel('Frequency (Hz)')
+plt.xlabel('Time (sec)')
 plt.yscale('log')
 plt.ylim([10, 10**3])
 plt.draw()
@@ -50,20 +54,21 @@ f, t, Sxx = signal.spectrogram(noise_data, sampF, 'hann', 100, 90)
 
 plt.figure(3)
 plt.pcolormesh(t, f, np.log10(Sxx))
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+plt.ylabel('Frequency (Hz)')
+plt.xlabel('Time (sec)')
 plt.yscale('log')
 plt.ylim([10, 10**3])
 plt.draw()
 
 plt.figure(4)
-f, Pxx_den = signal.periodogram(wave_data, sampF)
+f, Pxx_den = signal.welch(wave_data, sampF)
 plt.loglog(f, Pxx_den)
-f, Pxx_den = signal.periodogram(noise_data, sampF)
+f, Pxx_den = signal.welch(noise_data, sampF)
 plt.loglog(f, Pxx_den)
-plt.xlim([10, 10**3])
+# plt.xlim([10, 10**3])
 plt.xlabel('Frequency [Hz]')
-plt.ylabel('PSD [strain**2/Hz]')
+plt.ylabel('PSD [strain^2/Hz]')
+plt.grid(True,'both')
 plt.draw()
 
 plt.show()
--- superClassify.py
+++ superClassify.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import os
 import itertools
+import time
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.decomposition import PCA
 
 def read_data(index):
 
@@ -11,12 +14,12 @@ def read_data(index):
 
     lines = f.read().split('\n')
     l = lines.__len__() - 1
-    time = np.zeros(l)
+    tim = np.zeros(l)
     wave_data = np.zeros(l)
     noise_data = np.zeros(l)
 
     for i in range(0, l):
-        time[i] = float(lines[i].split(' ')[0])
+        tim[i] = float(lines[i].split(' ')[0])
         wave_data[i] = float(lines[i].split(' ')[1])*10**23
 
     f.close()
@@ -28,70 +31,7 @@ def read_data(index):
 
     f.close()
 
-    return time, wave_data, noise_data
-
-
-def logReg(x, y, lamb):
-
-    w = np.zeros(x.shape[0])
-
-    l = sum(np.log(1 + np.exp(-y * np.dot(x.T, w)))) + lamb * np.dot(w.T, w)
-    print(l)
-
-
-    stepSize = 10**-8
-    N=10**5
-    for i in range(N):
-        deltaL = np.dot(-y*np.exp(-y * np.dot(x.T, w))/(1 + np.exp(-y * np.dot(x.T, w))), x.T) \
-                 + 2*lamb*np.sqrt(np.dot(w.T, w))
-        w = w - stepSize*deltaL
-        l = sum(np.log(1 + np.exp(-y * np.dot(x.T, w)))) + lamb*np.dot(w.T, w)
-        print(l)
-
-    return w
-
-
-x_train = np.zeros(4096)
-y_train = np.array(0)
-x_test = np.zeros(4096)
-y_test = np.array(0)
-
-for i in range(1, len(os.listdir('./Data Sets/'))):
-    if i<=len(os.listdir('./Data Sets/'))/2:
-        if os.path.isfile('Data Sets/signal' + str(i) + '.dat'):
-            time, wave_data, noise_data = read_data(i)
-
-        x_train = np.column_stack((x_train, wave_data))
-        y_train = np.append(y_train, 1)
-        x_train = np.column_stack((x_train, noise_data))
-        y_train = np.append(y_train, -1)
-
-    if i > len(os.listdir('./Data Sets/')) / 2:
-        if os.path.isfile('Data Sets/signal' + str(i) + '.dat'):
-            time, wave_data, noise_data = read_data(i)
-
-        x_test = np.column_stack((x_test, wave_data))
-        y_test = np.append(y_test, 1)
-        x_test = np.column_stack((x_test, noise_data))
-        y_test = np.append(y_test, -1)
-
-x_train = (x_train[:, 1:].T-np.mean(x_train[:, 1:], axis=1)).T
-x_test = (x_test[:, 1:].T-np.mean(x_test[:, 1:], axis=1)).T
-y_train = y_train[1:]
-y_test = y_test[1:]
-
-n=100
-trainErr = np.zeros(n)
-testErr = np.zeros(n)
-
-clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial', max_iter=1000).fit(x_train.T, y_train.ravel())
-
-print('Time Series')
-print('Training Accuracy: ', clf.score(x_train.T, y_train))
-print('Testing Accuracy: ', clf.score(x_test.T, y_test))
-
-print(clf.get_params())
-
+    return tim, wave_data, noise_data
 
 
 x_train = np.zeros(129)
@@ -102,7 +42,7 @@ y_test = np.array(0)
 for i in range(1, len(os.listdir('./Data Sets/'))):
     if i<=len(os.listdir('./Data Sets/'))/2:
         if os.path.isfile('Data Sets/signal' + str(i) + '.dat'):
-            time, wave_data, noise_data = read_data(i)
+            tim, wave_data, noise_data = read_data(i)
 
         f, P1 = signal.welch(noise_data)
         f, P2 = signal.welch(wave_data)
@@ -114,7 +54,7 @@ for i in range(1, len(os.listdir('./Data Sets/'))):
 
     if i > len(os.listdir('./Data Sets/')) / 2:
         if os.path.isfile('Data Sets/signal' + str(i) + '.dat'):
-            time, wave_data, noise_data = read_data(i)
+            tim, wave_data, noise_data = read_data(i)
 
         f, P1 = signal.welch(noise_data)
         f, P2 = signal.welch(wave_data)
@@ -124,44 +64,69 @@ for i in range(1, len(os.listdir('./Data Sets/'))):
         x_test = np.column_stack((x_test, np.log10(P1)))
         y_test = np.append(y_test, -1)
 
-x_train = (x_train[:, 1:].T-np.mean(x_train[:, 1:], axis=1)).T
-x_test = (x_test[:, 1:].T-np.mean(x_test[:, 1:], axis=1)).T
+
+def svd_plot(alg, index, name):
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = .02
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = alg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    Z = Z.reshape(xx.shape)
+
+    colors = itertools.cycle(["r", "b"])
+
+    plt.figure(index)
+    plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+    for i in range(len(x_train)):
+        plt.plot(pca.transform(x_train)[i, 0], pca.transform(x_train)[i, 1], '.', color=next(colors))
+
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.title(name)
+    plt.draw()
+
+
+x_train = (x_train[:, 1:].T-np.mean(x_train[:, 1:], axis=1))
+x_test = (x_test[:, 1:].T-np.mean(x_test[:, 1:], axis=1))
 y_train = y_train[1:]
 y_test = y_test[1:]
 
-# n=10
-# trainErr = np.zeros(n)
-# testErr = np.zeros(n)
-#
-# for i in range(n):
-#     w = logReg(x_train, y_train, i)
-#     trainErr[i] = 1 - sum(y_train == np.sign(np.dot(x_train.T,w))) / len(y_train)
-#     testErr[i] = 1 - sum(y_test == np.sign(np.dot(x_test.T, w))) / len(y_train)
-#     print(trainErr[i])
-#     print(testErr[i])
+start = time.time()
+
+logReg = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(x_train, y_train.ravel())
+
+trainTime = time.time() - start
+start = time.time()
+
+print('Logistic Regression')
+print('Training Accuracy: ', logReg.score(x_train, y_train))
+print('Testing Accuracy: ', logReg.score(x_test, y_test))
+print('Training Time: ', trainTime, ' s')
+print('Execution Time: ', time.time()-start, ' s')
+
+start = time.time()
+
+svmAlg = svm.SVC(gamma='scale').fit(x_train, y_train.ravel())
+
+trainTime = time.time() - start
+start = time.time()
+
+print('SVM')
+print('Training Accuracy: ', svmAlg.score(x_train, y_train))
+print('Testing Accuracy: ', svmAlg.score(x_test, y_test))
+print('Training Time: ', trainTime, ' s')
+print('Execution Time: ', time.time()-start, ' s')
 
 
-clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(x_train.T, y_train.ravel())
+pca = PCA(n_components=2).fit(x_train)
+X = pca.transform(x_train)
 
-print('Spectrum')
-print('Training Accuracy: ', clf.score(x_train.T, y_train))
-print('Testing Accuracy: ', clf.score(x_test.T, y_test))
+logReg = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y_train.ravel())
+svmAlg = svm.SVC(gamma='scale').fit(X, y_train.ravel())
 
-# plt.figure(1)
-# plt.loglog(f, x_train)
-# plt.draw()
-#
-# plt.figure(3)
-# colors = itertools.cycle(["r", "b"])
-# for i in range(len(x_train)):
-#
-#     plt.plot(x_train[51, i], x_train[121, i], '.', color=next(colors))
-#
-# plt.draw()
-#
-# plt.figure(2)
-# plt.plot(range(n), trainErr)
-# plt.plot(range(n), testErr)
-# plt.draw()
-#
-# plt.show()
+svd_plot(logReg, 1, 'Logistic Regression')
+svd_plot(svmAlg, 2, 'SVM')
+
+plt.show()
