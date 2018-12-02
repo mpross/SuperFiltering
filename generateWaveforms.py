f98442a5800cf806c77e3fb90a24ecffff7e09af
M.P.Ross <mpross2@uw.edu>
2018-12-02 15:19:41 -0800

    Added code to generate CBC waveforms and the generated waveforms.

--- generateWaveforms.py
+++ generateWaveforms.py
@@ -1,17 +1,26 @@
 import pylab
 from pycbc.waveform import get_td_waveform
 
-for apx in ['SEOBNRv2', 'IMRPhenomC']:
-    hp, hc = get_td_waveform(approximant=apx,
-                                 mass1=10,
-                                 mass2=10,
-                                 spin1z=0.9,
-                                 delta_t=1.0/4096,
-                                 f_lower=40)
+for mass_1 in range(10, 100, 1):
+    for mass_2 in range(10, 100, 1):
+        hp, hc = get_td_waveform(approximant='IMRPhenomC',
+                                     mass1=mass_1,
+                                     mass2=mass_2,
+                                     spin1z=0,
+                                     delta_t=1.0/4096,
+                                     f_lower=40)
 
-    pylab.plot(hp.sample_times, hp, label=apx)
+        f = open('CBC Waveforms/' + str(mass_1) + '_' + str(mass_2) + '.dat', 'w+')
 
-pylab.ylabel('Strain')
-pylab.xlabel('Time (s)')
-pylab.legend()
-pylab.show()
+        for j in range(len(hp.sample_times)):
+            f.write(str(hp.sample_times[j]) + ' ' + str(hp[j]) + '\n')
+
+        f.close()
+
+        # pylab.plot(hp.sample_times, hp, label=str(mass_1))
+
+
+# pylab.ylabel('Strain')
+# pylab.xlabel('Time (s)')
+# pylab.legend()
+# pylab.show()
